3a8a0506a8895e0b04a212641c9b07c13327414e
Exadra37 <exadra37@gmail.com>
2016-09-24 17:44:31 +0100

    --wip--

---
+++ src/centos7/install-vestacp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# @author Exadra37(Paulo Silva) <exadra37ingmailpointcom>
+# @since  2016/09/24
+# @link   http://exadra37.com
+# @link https://gitlab.com/exadra37-bash/centos-server
+
+set -e
+
+curl -O http://vestacp.com/pub/vst-install.sh
+
+bash vst-install.sh
+
+yum install htop
