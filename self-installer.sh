db1d31e8d6283be64ee8ae876e5e942e687e2cf5
Exadra37 <exadra37@gmail.com>
2016-10-01 21:04:51 +0100

    --wip--

--- self-installer.sh
+++ self-installer.sh
@@ -21,7 +21,7 @@ set -e
 
         for script_to_execute in "$@"
             do
-                bash -l "${script_dir}/src/${script_to_execute}"
+                bash -li "${script_dir}/src/${script_to_execute}"
         done
     }
 
