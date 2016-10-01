68d7cb6ef363dbea4472e1622965e46621a7ae8b
Exadra37 <exadra37@gmail.com>
2016-10-01 22:52:25 +0100

    --wip--

--- self-installer.sh
+++ self-installer.sh
@@ -21,7 +21,7 @@ set -e
         local script_to_execute="${1}"
         #for script_to_execute in "$@"
          #   do
-        bash < "${script_dir}/src/${script_to_execute}"
+        < "${script_dir}/src/${script_to_execute}"
         #done
     }
 
