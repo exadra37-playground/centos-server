3059c2089daae59da135abb215450597fa921ae2
Exadra37 <exadra37@gmail.com>
2016-10-08 12:24:58 +0100

    --wip--

--- src/packages/vestacp/install-vestacp.sh
+++ src/packages/vestacp/install-vestacp.sh
@@ -28,3 +28,27 @@
     source vst-install-rhel.sh
 
     source "${script_dir}/src/tasks/git/tracking-etc.sh" "Installed VestaCp."
+
+
+# Change Admin Password
+
+    # from https://github.com/serghey-rodin/vesta/blob/master/install/vst-install-rhel.sh#L1227
+    local ip=$(ip addr | grep 'inet ' | grep global | head -n1 | awk '{print $2}' | cut -f1 -d/)
+
+    local url="https://{$ip}:8083"
+
+    # vesta is not handling passwords in a secure way, once it sends them in clear text and is generating weak passwords
+    local password="$(cat /dev/urandom | tr -cd "[[:alnum:]]" | tr -d "[[:space:]]" | head -c ${1:-32})"
+
+    bash v-change-user-password admin $password
+
+    source "${script_dir}/src/tasks/git/tracking-etc.sh" "Updated Admin password for VestaCp."
+
+    printf "\nRevoking VestaCP Admin Password sent by email.\n"
+
+    printf "\nLogin Valid Credentials\n"
+    printf "\nURL: {$url}"
+    printf "\nUser: admin"
+    printf "\nNew Password: {$password}"
+
+    printf "\nOld Admin Password sent by Vesta Installation, over email in clear text is a clear violation of Best Security practices, therefore the above one must be used to login.\n"
